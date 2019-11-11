38761d97d40071adb96d7ab37dfbe1f0fff58472
Aziz Faozi <admin@labseni.com>
2019-11-11 22:14:29 +0700

    gfrak

--- main.cpp
+++ main.cpp
@@ -8,22 +8,65 @@
 
 /* Kamu croot banget deh */
 void tester_db(int i, QString check);
+/* for tutorial testing database */
+void tester_db_1();
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qInfo()<<"just for testing";
 
-
+    tester_db_1();
     return a.exec();
 }
 
 
+
+void tester_db_1(){
+    qInfo()<<"menjadi sampah";
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setUserName("trumon");
+    db.setPassword("tappingbox123");
+    db.setDatabaseName("Trumon");
+    db.setPort(3306);
+
+    /* try to connection */
+
+    if(!db.open())
+    {
+        qInfo() << "Could not open connection!";
+        qInfo() << db.lastError().text();
+    }
+    else
+    {
+        /* Try to command line simple */
+        QString cmd;
+        /* command prepare for execute */
+        cmd = "select * from NeiraIklanVer3 where id = :id";
+        QSqlQuery query;
+        query.prepare(cmd);
+        query.bindValue(":id", 1);
+        query.exec();
+        /* reading from variable */
+        QString value0 = query.value(0).toString();
+        qInfo()<<"Value 0 : "<<value0;
+        qInfo() << "Connected, lets make a query...";
+
+
+        db.close();
+        qInfo() << "Disconnected";
+
+    }
+}
 /*
  *      retval : void
  *      Description: to check connection with mysql
 */
 void tester_db(int i,  QString check){
+    qInfo()<<"index : "<<i;
+    qInfo()<<"Check string : "<<check;
     qInfo()<<"Function called";
     QString Hostname = "localhost";
     QString Username = "trumon";
